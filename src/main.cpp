026433457296334cc1c588e00aab04e00db31538
Sergio López <sergio@noos.global>
2023-11-01 02:33:10 +0100

    yaw and pitch map

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@
 
 #include <EnableInterrupt.h>
 
-#define pin_INT_Throttle 8  // Pin Throttle
-#define pin_INT_Yaw 7      // Pin Yaw
-#define pin_INT_Pitch 12   // Pin Pitch
-#define pin_INT_Roll 9   // Pin Roll
+#define pin_INT_Throttle 11  // Pin Throttle
+#define pin_INT_Yaw 10      // Pin Yaw       // Varill 7 timón
+#define pin_INT_Pitch 12   // Pin Pitch     // Varilla 11 profundidad
+#define pin_INT_Roll 9    // Pin Roll
+#define status_led 13    // Off: starting, Blink: calbration mode, On: Flight mode
 
 Adafruit_PWMServoDriver servos = Adafruit_PWMServoDriver(0x40);
 long loop_timer, tiempo_ejecucion;
@@ -17,7 +18,7 @@ int Vel = 100;
 int Time = 0;
 float P = 0.75;
 
-unsigned int pos0=172; // ancho de pulso en cuentas para pocicion 0°
+unsigned int pos0=172;    // ancho de pulso en cuentas para pocicion 0°
 unsigned int pos180=565; // ancho de pulso en cuentas para la pocicion 180°
 
 // Maps
@@ -36,13 +37,22 @@ int mapYawToServoPosition(int yawValue) {
 
   // Map the yawValue to a servo position between 0 and 180 degrees
   // Adjust the values (minInput, maxInput, minOutput, maxOutput) as needed for your specific case
-  servoPosition = map(yawValue, 1060, 1980, 0, 180);
+  servoPosition = map(yawValue, 970, 1980, 0, 180);
 
   // Ensure the servo position is within valid range
   servoPosition = constrain(servoPosition, 0, 180);
-
   return servoPosition;
 }
+
+int mapPitchToServoPosition(int pitchValue) {
+  int servoPosition;
+
+  servoPosition = map(pitchValue, 1104, 1930, 0, 180);
+
+  servoPosition = constrain(servoPosition, 0, 180);
+  return servoPosition;
+}
+
 
 // Interrupciones
 
@@ -77,7 +87,6 @@ volatile int RC_Yaw_raw;
 void INT_Yaw() {
   if (digitalRead(pin_INT_Yaw) == HIGH) Yaw_HIGH_us = micros();
   if (digitalRead(pin_INT_Yaw) == LOW)  RC_Yaw_raw  = micros() - Yaw_HIGH_us;
-  
 }
 
 // fin interrupciones
@@ -86,6 +95,7 @@ void INT_Yaw() {
 void setup() {
   servos.begin();  
   servos.setPWMFreq(60); //Frecuecia PWM de 60Hz o T=16,66ms
+
   // Declaración de interrupciones
   pinMode(pin_INT_Yaw, INPUT_PULLUP);                   // YAW
   enableInterrupt(pin_INT_Yaw, INT_Yaw, CHANGE);
@@ -96,30 +106,19 @@ void setup() {
   pinMode(pin_INT_Roll, INPUT_PULLUP);                  // ROLL
   enableInterrupt(pin_INT_Roll, INT_Roll, CHANGE);
 
+  pinMode(status_led, OUTPUT);
+
   Serial.begin(115200);
 }
 
 void loop() {
-  /*
-    delay(1000);
-
-    setServo(11,90);
- 
-    delay(1000);
-
-    delay(1000);
-
-    setServo(11,180);
- 
-    delay(1000);
-    */
+  digitalWrite(status_led, HIGH);
 
   while (micros() - loop_timer < 10000);
   tiempo_ejecucion = (micros() - loop_timer) / 1000;
   loop_timer = micros();
 
   // Monitor Serie
-  
   Serial.print(RC_Throttle_raw);
   Serial.print("\t");
   Serial.print(RC_Pitch_raw);
@@ -127,5 +126,7 @@ void loop() {
   Serial.print(RC_Roll_raw);
   Serial.print("\t");
   Serial.println(RC_Yaw_raw);
-  
+
+  setServo(7, mapYawToServoPosition(RC_Yaw_raw));
+  setServo(11, mapPitchToServoPosition(RC_Pitch_raw));
 }
